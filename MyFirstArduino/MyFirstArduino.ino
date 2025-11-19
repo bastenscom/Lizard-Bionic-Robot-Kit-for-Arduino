26ba91307f135e67596d90179dff255eaae7f82c
Patrick Basten <bastenpersonal@gmail.com>
2025-11-19 13:32:15 -0600

    Update Arduino code to use Adafruit NeoPixel library

--- MyFirstArduino/MyFirstArduino.ino
+++ MyFirstArduino/MyFirstArduino.ino
@@ -1,6 +1,6 @@
 /*  ===============================================================================================
     - Project:   My First Arduio
-    - Version Date:      2025/11/17a
+    - Version Date:      2025/11/19a
     - Author: Patrick Basten - Bastens.com 
     ===============================================================================================
     - Overview: This code was written for teaching arduino code
@@ -21,57 +21,54 @@
 #include <Servo.h>
 Servo MyServo;
 
-#include <FastLED.h> // written with version 3.10.3
-#define LED_PIN     7
-#define NUM_LEDS    4
-#define BRIGHTNESS  150  // 0-255, adjust as needed
-
-#define COLOR_ORDER RGB // important to set using calibration ino, may be different order per different manufactures
-CRGB leds[NUM_LEDS];
-
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN    7
+#define NUM_LEDS   4
+Adafruit_NeoPixel LEDstrip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 { 
   MyServo.attach(2);
 
-  FastLED.addLeds <WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  // Set global brightness (optional)
-  FastLED.setBrightness(BRIGHTNESS);
-  // Clear all LEDs to off
-  FastLED.clear();
-  FastLED.show(); 
-  
+  LEDstrip.begin(); // Initialize the NeoPixel library.
+  LEDstrip.setBrightness(255);
+  LEDstrip.show();  // Turn all pixels off (clear the strip).
 }
 
 void loop()
 {
-  MyServo.write(10);
-  FastLED.clear();
-  leds[0] = CHSV( HUE_GREEN, 255, BRIGHTNESS);
-  leds[1] = CHSV( HUE_YELLOW, 255, BRIGHTNESS);
-  leds[2] = CHSV( HUE_RED, 255, BRIGHTNESS);
-  leds[3] = CHSV( random8(), 255, BRIGHTNESS);
-  FastLED.show();
-  
+  MyServo.write(20);
+  LEDstrip.clear();
+  LEDstrip.setBrightness(150);
+  LEDstrip.setPixelColor(0, LEDstrip.Color(255, 0, 0));
+  LEDstrip.setPixelColor(1, LEDstrip.Color(255, 255, 0));
+  LEDstrip.setPixelColor(2, LEDstrip.Color(0, 255, 0));
+  LEDstrip.setPixelColor(3, LEDstrip.Color(0, 0, 255));
+  LEDstrip.show();
   delay(2000);
   
   MyServo.write(90);
-  FastLED.clear();
+  LEDstrip.clear();
+  LEDstrip.setBrightness(200);
   for (int i = 0; i < NUM_LEDS; i++) { // Example 3: Create a simple chasing effect
-    leds[i] = CRGB::Green;
-    FastLED.show();
+    LEDstrip.setPixelColor(i, LEDstrip.Color(0, 255, 0));
+    LEDstrip.show();
     delay(250);
-    leds[i] = CRGB::Black; // Turn off the previous LED
+    LEDstrip.clear();
   }
-  FastLED.show();
   delay(250);
   
-  MyServo.write(170);
-  fill_solid(leds, NUM_LEDS, CRGB::Blue);
-  FastLED.show();
-  
+  MyServo.write(160);
+  LEDstrip.clear();
+  LEDstrip.setBrightness(100);
+  LEDstrip.setPixelColor(0, LEDstrip.Color(0, 0, 255));
+  LEDstrip.setPixelColor(1, LEDstrip.Color(0, 0, 255));
+  LEDstrip.setPixelColor(2, LEDstrip.Color(0, 0, 255));
+  LEDstrip.setPixelColor(3, LEDstrip.Color(0, 0, 255));
+  LEDstrip.show();
   delay(2000);
 }
+
 
 
 
